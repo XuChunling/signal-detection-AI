cf108389cd9c35628f7eeebd562a20a0183fe7d9
XuChunling <xucl@sari.ac.cn>
2019-07-18 10:36:06 +0800

    Add files via upload

---
+++ mnist_to_lite.py
@@ -0,0 +1,27 @@
+# author xuchunling
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test)
+
+keras_file = "model/mnist.h5"
+tf.keras.models.save_model(model, keras_file)
+
+#converter = lite.TocoConverter.from_keras_model_file(keras_file)
+converter = tf.lite.TFLiteConverter.from_keras_model_file(keras_file)
+tflite_model = converter.convert()
